bb58f9da7337c074d092f0ec422ee19fe4c3f805
Esteban F&M <esteban.viniegra@fymmx.com>
2024-10-18 13:27:56 -0600

    cambios

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import uvicorn
+
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return FileResponse("landingpage.html")
+async def get_index():
+    return FileResponse("index.html")
+
+@app.get("/logo")
+async def get_logo():
+    return FileResponse("logo.jpg")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
